52215a4b15bed0c66193d96c818f18c0ec9dcdd2
Fafurin <ffafurin@mail.ru>
2022-07-18 14:01:58 +0300

    added some cosmetic changes

--- app/Http/Controllers/Admin/User/StoreController.php
+++ app/Http/Controllers/Admin/User/StoreController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\User\StoreRequest;
 use App\Jobs\StoreUserJob;
 
-
 class StoreController extends Controller
 {
     public function __invoke(StoreRequest $request)
--- app/Http/Controllers/Main/IndexController.php
+++ app/Http/Controllers/Main/IndexController.php
@@ -3,19 +3,11 @@
 namespace App\Http\Controllers\Main;
 
 use App\Http\Controllers\Controller;
-use App\Models\Post;
 
 class IndexController extends Controller
 {
     public function __invoke()
     {
-        $posts = Post::paginate(6);
-
-        // выводим 4 рандомных поста
-        $randomPosts = Post::get()->random(4);
-
-        // выводим 4 поста по наибольшему количеству лайков
-        $popularPosts = Post::withCount('likedUsers')->orderBy('liked_users_count', 'DESC')->get()->take(4);
-        return view('main.index', compact('posts', 'randomPosts', 'popularPosts'));
+        return redirect()->route('post.index');
     }
 }
